337102cf7f9350eb159485d11b80857592533727
Ariel <cervanteslibch@gmail.com>
2023-08-11 21:09:00 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+import Menu from './components/Menu'
+import Navbar from './components/Navbar'
+
+const Container = styled.div`
+  
+`;
+
+const Main = styled.div`
+  
+`;
+
+const Wrapper = styled.div`
+  
+`;
+
+function App() {
+
+    return (
+        <Container>
+            <Menu />
+            <Main>
+                <Navbar />
+                <Wrapper>
+                    Video cards
+                </Wrapper>
+            </Main>
+        </Container>
+    )
+}
+
+export default App
